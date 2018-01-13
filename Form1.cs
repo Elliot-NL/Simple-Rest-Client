f4e609b0293d1278f9ec65cd36471dfba835ad50
Elliot-NL <elliotisvery@hotmail.co.uk>
2018-01-13 17:16:57 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing_File___WinFormsREST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Makes HTTP web request, gets stream of data and reads data
+            //input: http://dry-cliffs-19849.herokuapp.com/users.json
+            //debugOutput("This is testing the output");
+
+            RestClient rClient = new RestClient();
+            rClient.endPoint = txtUrl.Text;
+            debugOutput("Rest Client created");
+            string strResponse = string.Empty;
+            strResponse = rClient.makeRequest();
+            debugOutput(strResponse);
+
+        }
+
+        private void debugOutput(string strDebugTest)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugTest + Environment.NewLine);
+                textResponse.Text = textResponse + strDebugTest + Environment.NewLine;
+                textResponse.SelectionStart = textResponse.TextLength;
+                textResponse.ScrollToCaret();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ RestClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Testing_File___WinFormsREST
+{
+    public enum httpVerb
+    {
+        GET,
+        POST,
+        PUT,
+        UPDATE
+    }
+
+    class RestClient
+    {
+        public string endPoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+
+        public RestClient()
+        {
+            endPoint = string.Empty;
+            httpMethod = httpVerb.GET;
+        }
+
+        public string makeRequest()
+        {
+            string strResponseValue = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+            request.Method = httpMethod.ToString();
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if(response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("Error code :" + response.ToString());
+                }
+
+                //Process the response stream: JSON, XML, HTML
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if(responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+                //End response StreamReader
+
+            }
+
+                return strResponseValue;
+        }
+
+    }
+}
